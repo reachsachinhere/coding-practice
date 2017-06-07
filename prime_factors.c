422aaca89fda1a5b9673b7f2a8f5e46d9166eab0
reachsachinhere <sachinyadav47561@gmail.com>
2017-06-07 16:22:26 +0530

    updated

--- prime_factors.c
+++ prime_factors.c
@@ -1,30 +1,43 @@
 #include<stdio.h>
+int is_prime( int i );
+
 int main()
-{
- int n,i,k;
- printf("Enter a number");
- scanf("%d",&n);
+ {
+   int n , i , k ;
+   printf ( "Enter a number less than 500000 :::" );
+   scanf ( "%d" , &n );
  
- 
- for(i=2;i<=n/2;i++)
-  {
-    if(n%i==0)
-    {
-      int p=0;
-      for(int j=2;j<=i/2;j++)
+   for ( i = 2; i <= n; i++ )
        {
-          if(i%j==0)
-              p++;
-           
+           if ( n % i == 0 && is_prime(i) )
+              {
+                printf ("%d ",i);
+                n = n/i; 
+                i--;
+              }
+         
+           else if ( i > n/2 )
+                   {
+                     printf( "%d ",n);
+                     break;
+                   }
+        }
+    
+   return 0;
+ }
+
+int is_prime( int i )
+ {
+    int j , p=0 ;
+    for( j = 2; j <= i/2; j++)
+       {
+         if ( i % j == 0 )
+            p++;
        }
-    if(p==0)
-     {
-      printf("%d ",i);
-      k++;
-     }
-   }
-  }
-if(k==0)
-printf("%d",n);
-return 0;
-}
+      
+    if ( p == 0 )
+       return 1;
+    else 
+       return 0;
+ }  
+           
