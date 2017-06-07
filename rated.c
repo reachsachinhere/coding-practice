3c40a0f23625975157de6667d2512546699b1452
reachsachinhere <sachinyadav47561@gmail.com>
2017-06-07 16:38:01 +0530

    updated

--- rated.c
+++ rated.c
@@ -2,35 +2,37 @@
 #include<stdlib.h>
 
 void main()
-{
- int a[1000],b[1000],n,i,p=0;
- //printf("enter the number of round participants n(2<=n<=1000)");
- scanf("%d",&n);
- for(i=0;i<n;i++)
  {
-   scanf("%d %d",&a[i],&b[i]);
- }
- 
- for(i=0;i<n;i++)
- {
-  if(a[i]!=b[i])
-   {
+    int a[1000] , b[1000] , n , i , p = 0;
+    printf(" enter the number of round participants n ( 2 < = n < = 1000 ) ");
+    scanf( "%d" , &n );
     
-     printf("rated ");
-     exit(0);
-    }
+    for ( i = 0; i < n; i++ )
+      {
+         scanf ( "%d%d" , &a[i], &b[i] );
+      }
+ 
+    for ( i = 0; i < n; i++ )
+       {
+         if ( a[i]!=b[i] )
+           {
+             printf("rated ");
+             exit(0);
+           }
    
-   else if(i<n-1&&a[i]<a[i+1])
-          p++;
+          else if ( i < n-1 && a[i] < a[i+1] )
+                 p++;
    
-  }
+       }
 
-  if(i==n&&p>0)
-  printf("unrated ");
-  else
-  printf("maybe ");
- exit(0);
-} 
+     if ( i == n && p > 0 )
+     printf("unrated ");
+    
+     else
+         printf("maybe ");
+ 
+    exit(0);
+ } 
   
  
  
