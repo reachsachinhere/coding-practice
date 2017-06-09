47a9e21c9257092997a78d7f89821fb21a166a18
reachsachinhere <sachinyadav47561@gmail.com>
2017-06-09 10:39:14 +0530

    updated

--- largest_palindrome_product.c
+++ largest_palindrome_product.c
@@ -1,3 +1,7 @@
+/*A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two n-digit numbers.*/
+
 #include<stdio.h>
 
 int largest_palindrome( int n);
@@ -16,31 +20,31 @@ int main()
 int largest_palindrome( int n)
 {
    int b = 1;
-   for ( int i = 0; i < n; i++ )
+   for ( int i = 0; i < n; i++ )                   //it is to find the largest and smallest number possible for given no of digits
       {
         b=b*10;
       }
       int lower_limit = b / 10;
       int upper_limit = b - 1;
-      int max = 1;
+      int max = 1;                             //max is the largest palindrome number
 
-   for ( int i = upper_limit; i >= lower_limit; i-- )
+   for ( int i = upper_limit; i >= lower_limit; i-- ) //loop for finding the largest palindrome no
     {
-      if( i*i < max )
+      if( i*i < max ) //i is decremented with every loop.So if i^2 is less than max then there is no need to continue
         {  
           return max;
         } 
       
-      for (int j = i; j >= lower_limit; j-- )
+      for (int j = i; j >= lower_limit; j-- ) //loop for multiplying i with every number less than it and checking it if it is palindrome
           { 
             int a = j * i;
-            if ( a < max )
+            if ( a < max ) //j is decremented with every loop.So there is no need to continue if a < max.
                break;
             
             if( ispalindrome(a) && a > max )
               {
                 max = a;
-                break;
+                break;                       
               }
        
           }
@@ -48,7 +52,7 @@ int largest_palindrome( int n)
 }
 
 
-int ispalindrome( int a )
+int ispalindrome( int a )           //this function tells whether the function is palindrome or not
 { 
   int p = a, q = 0;
   while ( p != 0 )
